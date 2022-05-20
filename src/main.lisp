71ffb043f9d8190bfe72e7656b430d94acefbcdf
Sammi Johnson <sambjohnson94@gmail.com>
2022-05-19 18:41:13 -0700

    First working demo

--- src/main.lisp
+++ src/main.lisp
@@ -3,6 +3,24 @@
   (:export :main))
 (in-package :cl-gtk-hello-world)
 
+; Main window
+(defvar window (make-instance 'gtk:gtk-window :type :toplevel :title "Hello world! :3"))
+(defvar vbox (make-instance 'gtk:gtk-box :orientation :vertical
+                                         :spacing 25
+                                         :margin 25))
+
+(defun hello-world ()
+  "Creates a hello world window in GTK+3"
+  (gtk:within-main-loop
+   ; Quit program when window closed
+   (gobject:g-signal-connect window "destroy" (lambda (widget)
+                                               (declare (ignore widget))
+                                               (gtk:leave-gtk-main)))
+   ; Display GUI
+   (gtk:gtk-container-add window vbox)
+   (gtk:gtk-widget-show-all window))
+  (gtk:join-gtk-main))
+
 (defun hello (name)
   (format t "This is the start of something great, ~a!~%" name))
 
@@ -16,7 +34,8 @@
   (when (member "-h" argv :test #'equal)
     (help)
     (uiop:quit))
-  (hello "Sam Johnson (she/her)"))
+  (hello "Sam Johnson (she/her)")
+  (hello-world))
 
 (defun main ()
   "Entry point for the executable.
